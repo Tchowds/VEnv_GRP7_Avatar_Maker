2e8cb716e38c6ba5d79125e84df945b459a44c41
Fabian <fabian9876@ntlworld.com>
2025-02-28 01:25:00 +0000

    Added door auto close

--- Assets/Resources/Doors/DoorController.cs
+++ Assets/Resources/Doors/DoorController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class DoorController : MonoBehaviour
 {
@@ -6,15 +8,17 @@ public class DoorController : MonoBehaviour
     [SerializeField] private float openAngle = 90f;  // Maximum door open angle (can be set in the Inspector)
     [SerializeField] private float closedAngle = 0f; // Default closed angle (can be set in the Inspector)
     [SerializeField] private float speed = 90f;  // Speed at which the door opens and closes
+    [SerializeField] private float closeDelay = 3f;  // Time to wait before closing the door automatically
 
     private float currentAngle = 0f;  // Current angle of the door
     private bool isOpening = false;   // Whether the door is in the process of opening
     private float targetAngle = 0f;   // The target angle for the door to reach
+    private bool doorOpen = false;    // To track if the door is currently open
 
     void Start()
     {
         // Set the initial door angle to the closed angle
-        currentAngle = openAngle;
+        currentAngle = closedAngle;
     }
 
     void Update()
@@ -41,5 +45,29 @@ public class DoorController : MonoBehaviour
         Debug.Log("Toggling door state!");
         isOpening = !isOpening;
         targetAngle = isOpening ? openAngle : closedAngle;  // Toggle between open and closed
+        doorOpen = isOpening;
+
+        // Stop any existing coroutine and start the auto-close coroutine
+        StopAllCoroutines();
+        if (doorOpen)
+        {
+            StartCoroutine(AutoCloseDoor());
+        }
+    }
+
+    // Coroutine to automatically close the door after a set delay
+    private IEnumerator AutoCloseDoor()
+    {
+        // Wait for the specified duration (closeDelay)
+        yield return new WaitForSeconds(closeDelay);
+
+        // Automatically close the door if it hasn't been manually closed yet
+        if (doorOpen)
+        {
+            Debug.Log("Automatically closing the door.");
+            isOpening = false;
+            targetAngle = closedAngle;
+            doorOpen = false;
+        }
     }
 }
