10613463054d1c64fe763e9efb47c8364ee6b440
Fabian <fabian9876@ntlworld.com>
2025-02-28 03:28:48 +0000

    adjust sound mixing

--- Assets/Resources/Music/ShopMusicManager.cs
+++ Assets/Resources/Music/ShopMusicManager.cs
@@ -10,8 +10,8 @@ public class ShopMusicManager : MonoBehaviour
     void Start()
     {
         // Set the crowd sound as the default
-        audioMixer.SetFloat("CrowdVolume", 0);  // Loudest (adjust to your needs)
-        audioMixer.SetFloat("ShopVolume", -30); // Mute or set to a very low level
+        audioMixer.SetFloat("CrowdVolume", -15);  // Loudest (adjust to your needs)
+        audioMixer.SetFloat("ShopVolume", -50); // Mute or set to a very low level
 
        
     }
@@ -19,15 +19,15 @@ public class ShopMusicManager : MonoBehaviour
     public void EnterShop()
     {
         // Fade in shop sound and fade out crowd sound
-        StartCoroutine(FadeSound("CrowdVolume", -20, 1f)); // Fade out crowd
-        StartCoroutine(FadeSound("ShopVolume", 0, 1f));   // Fade in shop music
+        StartCoroutine(FadeSound("CrowdVolume", -40, 1f)); // Fade out crowd
+        StartCoroutine(FadeSound("ShopVolume", -15, 1f));   // Fade in shop music
     }
 
     public void ExitShop()
     {
         // Fade in crowd sound and fade out shop music
-        StartCoroutine(FadeSound("CrowdVolume", 0, 1f));   // Fade in crowd
-        StartCoroutine(FadeSound("ShopVolume", -30, 1f));  // Fade out shop music
+        StartCoroutine(FadeSound("CrowdVolume", -15, 1f));   // Fade in crowd
+        StartCoroutine(FadeSound("ShopVolume", -50, 1f));  // Fade out shop music
     }
 
     private IEnumerator FadeSound(string parameterName, float targetVolume, float duration)
