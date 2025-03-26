a0bf2c6685d9a49d122f3e10a40ca7a05e04d935
Tchowds <zcabtac@ucl.ac.uk>
2025-03-26 05:23:12 +0000

    Barrier operator cleaned

--- Assets/Resources/Avatar Barriers/BarrierOperator.cs
+++ Assets/Resources/Avatar Barriers/BarrierOperator.cs
@@ -19,6 +19,15 @@ public class JsonDataWrapper
     public int[][] hands;
 }
 
+/// <summary>
+/// DEPRECATED - CUT FROM FINAL DEMO
+/// 
+/// This class is attached to the AvatarBarrier Prefab and is responsible for detecting when the user interacting with it has the same texture body parts as the barrier.
+/// There are two modes of operation: single avatar and dual avatar which define how many skins need to be matched to activate the barrier and unlock it
+/// In dual avatar mode the skins need to be matched in quick succession.
+/// The object is networked to operate over ubiq rooms.
+/// 
+/// </summary>
 public class BarrierOperator : MonoBehaviour
 {
 
@@ -36,22 +45,23 @@ public class BarrierOperator : MonoBehaviour
     private NetworkContext context;
     private float lastDownTime = 0.0f;
 
-
+    // Barrier message defining the state of the barrier in terms of which skins need to be matched
     private struct BarrierMessage
     {
         public bool primaryActive;
         public bool secondaryActive;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Set the entirety of the barrier to be interactable
         interactable = GetComponentInChildren<XRSimpleInteractable>();
         if (interactable) interactable.selectEntered.AddListener(Interactable_SelectEntered_Match_Avatar);
         
         primaryAvatar = transform.Find("PrimaryAvatar")?.gameObject;
         SecondaryAvatar = transform.Find("SecondaryAvatar")?.gameObject;
 
+        // Position avatars on the barrier
         if (dualAvatars)
         {
             primaryAvatar.transform.localPosition = new Vector3(-0.2f, 0, -0.6f);
@@ -70,11 +80,21 @@ public class BarrierOperator : MonoBehaviour
         context = NetworkScene.Register(this);
     }
 
+
+    void Update()
+    {
+        // Send message to reset the barrier if the user has not matched the skins in time in dual avatar mode
+        if (dualAvatars && (!primaryAvatar.activeSelf || !SecondaryAvatar.activeSelf))
+        {
+            if (Time.time - lastDownTime > countdown) setActiveAndSendMessage(true, true);
+        }
+    }
+
+    // Parse the JSON file to get the overrides which define sets of matching skin parts on different skins
     public void parseJson()
     {
-        // Use Newtonsoft.Json to deserialize the JSON
         JsonDataWrapper data = JsonConvert.DeserializeObject<JsonDataWrapper>(jsonFile.text);
-        Debug.Log(data.head);
+
 
         overrides = new Dictionary<string, List<HashSet<int>>>();
         overrides.Add("head", new List<HashSet<int>>());
@@ -106,31 +126,51 @@ public class BarrierOperator : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (dualAvatars && (!primaryAvatar.activeSelf || !SecondaryAvatar.activeSelf))
-        {
-            if (Time.time - lastDownTime > countdown) setActiveAndSendMessage(true, true);
-        }
-    }
-
+    // Set the textures up on the barrier avatars
     void setupAvatarTextures(GameObject avatar, int[] avatarParts)
     {
-    // Retrieve the required components
     var floating = avatar.GetComponentInChildren<FloatingAvatarSeparatedTextures>();
     var textured = avatar.GetComponent<TexturedAvatar>();
 
-    // Define the avatar parts in the desired order.
     AvatarPart[] parts = { AvatarPart.HEAD, AvatarPart.TORSO, AvatarPart.LEFTHAND, AvatarPart.RIGHTHAND };
 
-    // Loop through each part and set the corresponding texture.
-    for (int i = 0; i < parts.Length; i++)
-    {
-        floating.avatarPart = parts[i];
-        textured.SetTexture(avatarParts[i].ToString());
-    }
+        for (int i = 0; i < parts.Length; i++)
+        {
+            floating.avatarPart = parts[i];
+            textured.SetTexture(avatarParts[i].ToString());
+        }
     }
 
+    // Send a message defining the state of the two avatars in the barrier
+    private void setActiveAndSendMessage(bool primary, bool secondary)
+    {
+        gameObject.SetActive(primary || secondary);
+        primaryAvatar.SetActive(primary);
+        SecondaryAvatar.SetActive(secondary);
+        lastDownTime = Time.time;
+
+        context.SendJson(new BarrierMessage()
+        {
+            primaryActive = primary,
+            secondaryActive = secondary
+        });
+    }
+
+    // Process the message to set the state of the barrier and updates it
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var m = message.FromJson<BarrierMessage>();
+        bool remotePrimaryActive = m.primaryActive;
+        bool remoteSecondaryActive = m.secondaryActive;
+        if(!remotePrimaryActive && !remoteSecondaryActive) gameObject.SetActive(false);
+        else
+        {
+            primaryAvatar.SetActive(remotePrimaryActive);
+            SecondaryAvatar.SetActive(remoteSecondaryActive);
+        }
+    }
+
+    // Adds interactable listener that checks the player avatar to the barrier avatars and adjusts state if macthing
     private void Interactable_SelectEntered_Match_Avatar(SelectEnterEventArgs arg0)
     {
         var networkScene = NetworkScene.Find(this);
@@ -161,20 +201,9 @@ public class BarrierOperator : MonoBehaviour
         }
     }
 
-    private void setActiveAndSendMessage(bool primary, bool secondary)
-    {
-        gameObject.SetActive(primary || secondary);
-        primaryAvatar.SetActive(primary);
-        SecondaryAvatar.SetActive(secondary);
-        lastDownTime = Time.time;
 
-        context.SendJson(new BarrierMessage()
-        {
-            primaryActive = primary,
-            secondaryActive = secondary
-        });
-    }
 
+    // Check if the avatar has the same texture as the barrier
     private bool isSameAvatar(AvatarTextureCatalogue catalogue, int[] modelParts, FloatingAvatarSeparatedTextures avatar)
     {
         Texture[] avatarTextures = { avatar.headRenderer.material.mainTexture,
@@ -191,13 +220,13 @@ public class BarrierOperator : MonoBehaviour
         return true;
     }
 
+    // An extra matching step to see if the textures are in the same set of overrides which are defined if skin parts across skins are the same
     private bool matchOverrides(AvatarTextureCatalogue catalogue, Texture modelTexture, Texture avatarTecture, List<HashSet<int>> overrideSets)
     {
         {
             int modelIndex = -1;
             int avatarIndex = -1;
             
-            // Assuming catalogue.Textures is an array or list of Texture objects.
             for (int i = 0; i < catalogue.Textures.Count; i++)
             {
                 if (catalogue.Textures[i] == modelTexture) modelIndex = i;
@@ -215,21 +244,9 @@ public class BarrierOperator : MonoBehaviour
         }
     }
 
+    // Remove listener on interactable when object is destroyed
     private void OnDestroy()
     {
         if (interactable) interactable.selectEntered.RemoveListener(Interactable_SelectEntered_Match_Avatar);
-    }
-
-    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
-    {
-        var m = message.FromJson<BarrierMessage>();
-        bool remotePrimaryActive = m.primaryActive;
-        bool remoteSecondaryActive = m.secondaryActive;
-        if(!remotePrimaryActive && !remoteSecondaryActive) gameObject.SetActive(false);
-        else
-        {
-            primaryAvatar.SetActive(remotePrimaryActive);
-            SecondaryAvatar.SetActive(remoteSecondaryActive);
-        }
     }
 }
